25738b815ad191b95412451668df485a88f2b2ab
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-04-05 16:49:52 -0700

    make it bloggy

--- src/app.lua
+++ src/app.lua
@@ -1,17 +1,29 @@
-require './app.css'
+require './base.css'
 
 local React = require 'react'
 
+-- Convert a Lua value `val` to a JavaScript value
+local function JS(val)
+    if type(val) == 'table' then
+        local o = js.new(js.global.Object)
+        for k, v in pairs(val) do
+            if type(k) == 'string' then
+                o[k] = JS(v)
+            end
+        end
+        return o
+    end
+    return val
+end
+
+-- A table of 'tags' for React component types
 local R = setmetatable({}, {
     __index = function(r, elementType)
         local tag = function(t)
-            local props = js.new(js.global.Object)
-            for k, v in pairs(t) do
-                if type(k) == 'string' then
-                    props[k] = v
-                end
+            if type(t) ~= 'table' then
+                return React:createElement(elementType, js.null, t)
             end
-            return React:createElement(elementType, props, table.unpack(t))
+            return React:createElement(elementType, JS(t), table.unpack(t))
         end
         r[elementType] = tag -- Cache it
         return tag
@@ -22,29 +34,47 @@ local logo = require './logo.svg'
 
 local App = function()
     return R.div {
-        className = 'App',
-        R.header {
-            className = 'App-header',
-            R.img {
-                src = logo,
-                className = 'App-logo',
-                alt = 'logo'
-            },
-            R.h1 {
-                className = 'App-title',
-                'Welcome to React',
-            },
+        R.p {
+            style = { textAlign = 'right' },
+            R.a { href = '/', 'home' },
         },
-        R.button {
-            onClick = function()
-                js.global:alert('clicked!')
-            end,
-            'Click me! :)',
+        R.h2 'Welcome to React',
+        R.div {
+            className = 'date',
+            '2018-04-05',
+        },
+        R.img {
+            src = logo,
+            alt = 'logo'
+        },
+        R.div {
+            style = {
+                display = 'flex',
+                justifyContent = 'center',
+            },
+            R.button {
+                style = { textAlign = 'center' },
+                onClick = function()
+                    js.global:alert('clicked!')
+                end,
+                'Click me! :)',
+            },
         },
         R.p {
-            className = 'App-intro',
-            'To get started, edit ', R.code { 'src/test.lua' }, ' and save to reload.',
+            'To get started, edit ', R.code 'src/test.lua', ' and save to reload.',
         },
+        R.div {
+            className = 'content-footer',
+            R.ul {
+                R.li 'one',
+                R.li 'two',
+            },
+        },
+        R.ol {
+            className = 'footnotes',
+            R.li 'omg',
+            R.li 'kk',
+        }
     }
 end
 
